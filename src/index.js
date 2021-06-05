10b24c88b1515f33ee070028176754e1f20f5bd4
Amir Khan <ak9471714@gmail.com>
2021-06-05 14:02:10 +0530

    Add reducers and store

--- src/index.js
+++ src/index.js
@@ -2,6 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
+import {configureStore} from './store';
+
+const store = configureStore();
+console.log('store', store);
+console.log('state', store.getState());
+
 
 ReactDOM.render(
   <React.StrictMode>
