ceddc70284b2bf017dae7c28a7396a432b59cc02
Amir Khan <ak9471714@gmail.com>
2021-06-21 13:46:33 +0530

    Loggin user - create action type and reducer for loggin in

---
+++ src/reducers/auth.js
@@ -0,0 +1,34 @@
+import {LOGIN_FAILED, LOGIN_START, LOGIN_SUCCESS}  from '../actions/actionTypes';
+
+const initialAuthState = {
+    user: {},
+    error: null,
+    isLoggedIn: false,
+    inProgress: false,
+};
+
+export default function auth (state = initialAuthState, action){
+    switch (action.type) {
+        case LOGIN_START:
+            return {
+                ...state,
+                inProgress: true
+            };
+        case LOGIN_SUCCESS:
+            return {
+                ...state,
+                user: action.user,
+                isLoggedIn: true,
+                error: null,
+                inProgress: false,
+            };
+        case LOGIN_FAILED:
+            return {
+                ...state,
+                error: action.error,
+                inProgress: false,
+            };
+        default:
+            break;
+    }
+}
