be92413c79b89ed177115bd4b4e13edb5ef3c629
Amir Khan <ak9471714@gmail.com>
2021-06-21 09:18:39 +0530

    Fetch posts from an api

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,28 @@
+// import React from 'react';
 import React from 'react';
+import { connect } from 'react-redux';
 
-function App() {
-    return <div className="App">App</div>
+import { fetchPosts } from '../actions/posts';
+
+class App extends React.Component {
+  // to fetch the post from an api
+  componentDidMount() {
+    // dispatch an async action to fetch posts from an api
+    // and store those posts in the redux store
+    this.props.dispatch(fetchPosts());
+  }
+
+  render() {
+    console.log('PROPS', this.props);
+    return <div>APP</div>;
+  }
 }
 
-export default App;
+function mapStateToProps(state) {
+  return {
+    posts: state.posts,
+  };
+}
+
+// connect our app componnet to the redux store
+export default connect(mapStateToProps)(App);
