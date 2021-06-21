c10c48cf9b27af0e6e6620136c7e46e39c45840f
Amir Khan <ak9471714@gmail.com>
2021-06-21 09:32:54 +0530

    Add UI for posts

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 import { fetchPosts } from '../actions/posts';
+import { PostsList } from './';
 
 class App extends React.Component {
   // to fetch the post from an api
@@ -13,8 +14,12 @@ class App extends React.Component {
   }
 
   render() {
-    console.log('PROPS', this.props);
-    return <div>APP</div>;
+    const { posts } = this.props;
+    return (
+      <div>
+        <PostsList posts={posts} />
+      </div>
+    );
   }
 }
 
