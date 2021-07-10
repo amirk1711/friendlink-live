709ad903d17fbc1e393b96b0a1a50d4f1fcde85e
Amir Khan <ak9471714@gmail.com>
2021-07-10 23:53:11 +0530

    Change UI

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,17 +1,23 @@
 import React from 'react';
 
 function Comment({ comment }) {
-  return (
-    <div className="post-comment-item">
-      <div className="post-comment-header">
-        <span className="post-comment-author">{comment.user.name}</span>
-        <span className="post-comment-time">a minute ago</span>
-        <span className="post-comment-likes">{comment.likes.length} likes</span>
-      </div>
+    return (
+        <div className="post-comment-item">
+            <div className="post-comment-author-profile">
+                <img src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cmFuZG9tJTIwcGVvcGxlfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80" />
+            </div>
 
-      <div className="post-comment-content">{comment.content}</div>
-    </div>
-  );
+            <div className="post-comment">
+                <div className="post-comment-details">
+                    <p className="post-comment-author-name">{comment.user.name}</p>
+                    <p className="post-comment-content">{comment.content}</p>
+                </div>
+                <p className="post-comment-action">
+                    <span>2h&emsp;{comment.likes.length} Likes&emsp;Reply</span>
+                </p>
+            </div>
+        </div>
+    );
 }
 
 export default Comment;
--- src/components/Post.js
+++ src/components/Post.js
@@ -4,6 +4,12 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { Comment } from './';
 import { addLike, createComment } from '../actions/posts';
+import {
+    CommentOutlined,
+    FavoriteBorder,
+    Favorite,
+    MoreVert,
+} from '@material-ui/icons';
 
 class Post extends Component {
     constructor(props) {
@@ -44,69 +50,96 @@ class Post extends Component {
 
         const isPostLikedByUser = post.likes.includes(user._id);
         return (
-            <div className="post-wrapper" key={post._id}>
-                <div className="post-header">
-                    <div className="post-avatar">
-                        <Link to={`/user/${post.user._id}`}>
-                            <img
-                                src="https://image.flaticon.com/icons/svg/2154/2154651.svg"
-                                alt="user-pic"
-                            />
-                        </Link>
-                        <div>
-                            <span className="post-author">
-                                {post.user.name}
-                            </span>
-                            <span className="post-time">a minute ago</span>
+            <div className="post">
+                <div className="post-wrapper" key={post._id}>
+                    <div className="post-top">
+                        <div className="post-top-left">
+                            <Link to={`/user/${post.user._id}`}>
+                                <img
+                                    src="https://image.flaticon.com/icons/svg/2154/2154651.svg"
+                                    alt="user-pic"
+                                    className="post-profile-image"
+                                />
+                            </Link>
+                            <div className="post-details">
+                                <span className="post-author">
+                                    {post.user.name}
+                                </span>
+                                <span className="post-time">7m ago</span>
+                            </div>
+                        </div>
+
+                        <div className="post-top-right">
+                            <MoreVert />
                         </div>
                     </div>
-                    <div className="post-content">{post.content}</div>
 
-                    <div className="post-actions">
-                        <button
-                            className="post-like no-btn"
-                            onClick={this.handlePostLike}
-                        >
-                            {isPostLikedByUser ? (
-                                <img
-                                    src="https://image.flaticon.com/icons/svg/1076/1076984.svg"
-                                    alt="likes-icon"
-                                />
-                            ) : (
-                                <img
-                                    src="https://image.flaticon.com/icons/svg/1077/1077035.svg"
-                                    alt="likes-icon"
-                                />
-                            )}
-
-                            <span>{post.likes.length}</span>
-                        </button>
-
-                        <div className="post-comments-icon">
-                            <img
-                                src="https://image.flaticon.com/icons/svg/1380/1380338.svg"
-                                alt="comments-icon"
-                            />
-                            <span>{post.comments.length}</span>
-                        </div>
-                    </div>
-                    <div className="post-comment-box">
-                        <input
-                            placeholder="Start typing a comment"
-                            onChange={this.handleOnCommentChange}
-                            onKeyPress={this.handleAddComment}
-                            value={comment}
+                    <div className="post-center">
+                        <img
+                            src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cmFuZG9tJTIwcGVvcGxlfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80"
+                            className="post-image"
+                            alt="post-img"
                         />
+                        <div className="post-actions">
+                            <button
+                                className="post-like no-btn"
+                                onClick={this.handlePostLike}
+                            >
+                                {isPostLikedByUser ? (
+                                    <Favorite style={{ color: 'red' }} />
+                                ) : (
+                                    <FavoriteBorder />
+                                )}
+
+                                <span>{post.likes.length}</span>
+                            </button>
+
+                            <button className="post-comments no-btn">
+                                <CommentOutlined />
+                                <span>{post.comments.length}</span>
+                            </button>
+                        </div>
+                        <div className="liked-by-detail">
+                            <img src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cmFuZG9tJTIwcGVvcGxlfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80" />
+                            <p>
+                                Liked by <span>amirk1711</span> and{' '}
+                                <span>3 others</span>
+                            </p>
+                        </div>
+                        <div className="post-caption">
+                            <p>
+                                <span>amirkhann.17</span>
+                                &nbsp; Hey! This is my first post. Hey! This is
+                                my first post. Hey! This is my first post. Hey!
+                                This is my first post. Hey! This is my first
+                                post.Hey! This is my first post.
+                            </p>
+                        </div>
                     </div>
 
-                    <div className="post-comments-list">
-                        {post.comments.map((comment) => (
-                            <Comment
-                                comment={comment}
-                                key={comment._id}
-                                postId={post._id}
+                    <div className="post-bottom">
+                        <p className="view-comments">View all 7 comments</p>
+
+                        <div className="post-comments-list">
+                            {post.comments.map((comment) => (
+                                <Comment
+                                    comment={comment}
+                                    key={comment._id}
+                                    postId={post._id}
+                                />
+                            ))}
+                        </div>
+
+                        <div className="post-comment-box">
+                            <input
+                                placeholder="Start typing a comment"
+                                onChange={this.handleOnCommentChange}
+                                onKeyPress={this.handleAddComment}
+                                value={comment}
                             />
-                        ))}
+                            
+                            <button onSubmit={this.handleAddComment} className="add-comment-btn">Post</button>
+                        </div>
                     </div>
                 </div>
             </div>
