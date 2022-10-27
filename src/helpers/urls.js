ea2b14f59f0308149fff17cfceb78584fc67bdf4
Amir Khan <ak9471714@gmail.com>
2022-10-27 05:31:15 +0530

    change backend url

--- src/helpers/urls.js
+++ src/helpers/urls.js
@@ -1,4 +1,4 @@
-const API_ROOT = 'https://friendlink-app.herokuapp.com/api/v1';
+const API_ROOT = 'https://friendlink.onrender.com/api/v1';
 
 export const APIUrls = {
     login: () => `${API_ROOT}/users/login`,
